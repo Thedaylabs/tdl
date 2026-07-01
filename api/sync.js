375e4482c12408fc508b7a07f2bb84f68c51f451
nahyuntdl <nahyun@thedaylabs.com>
2026-07-01 10:47:12 +0900

    fix: 관리자 페이지 GAS URL을 지점 제출 서버와 동일하게 수정 (데이터 미노출 버그)

--- api/sync.js
+++ api/sync.js
@@ -1,6 +1,6 @@
 export const config = { api: { bodyParser: false, responseLimit: '20mb' } };
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbye2uaGpCexhNKivxrQf6givnzkIMUNdJVSny9GOTfmnHT0A5QvIKRIFcbbPU_m9vJz/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwgvA5ZB2ROdBTo4f_7N-GXTezd4UhH5kTWlIKInmlVrugTONgzApw5Ee4xL4orb0W_/exec';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
